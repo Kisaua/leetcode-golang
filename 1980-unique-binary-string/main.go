48fbde23722cf1cf41483a82dc4982d056b972a8
rudenkiy <sergiy.rudenkiy@digits.schwarzz>
2026-03-19 12:20:42 +0100

    1980 unique binary string
    initial implementation

---
+++ 1980-unique-binary-string/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+	"strconv"
+)
+
+func main() {
+	findDifferentBinaryString([]string{"01", "10"})
+}
+
+func findDifferentBinaryString(nums []string) string {
+
+	n := len(nums)
+
+	for i := 0; i < n+1; i++ {
+		s := fmt.Sprintf("%%0%ds", n)
+		binaryString := fmt.Sprintf(s, strconv.FormatInt(int64(i), 2))
+
+		//		fmt.Println(binaryString, s)
+		if !slices.Contains(nums, binaryString) {
+			return binaryString
+
+		}
+
+	}
+
+	return ""
+}
