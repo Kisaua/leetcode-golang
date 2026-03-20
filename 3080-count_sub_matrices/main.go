9320c86e3ce080ca6cb525cf4ab95fd316fdff94
rudenkiy <sergiy.rudenkiy@digits.schwarzz>
2026-03-20 18:05:27 +0100

    solution 3080

---
+++ 3080-count_sub_matrices/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+}
+
+func countSubmatrices(grid [][]int, k int) int {
+	count := 0
+
+	for i := 0; i < len(grid); i++ {
+		fmt.Println(i, grid)
+		for j := 0; j < len(grid[i]); j++ {
+			left := 0
+			top := 0
+			if j != 0 {
+				left = grid[i][j-1]
+			}
+			if i != 0 {
+				top = grid[i-1][j]
+			}
+			if grid[i][j]+left+top <= k {
+				count++
+				grid[i][j] += left + top
+			}
+		}
+	}
+
+	return count
+}
