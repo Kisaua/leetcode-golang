5d344cd9b53bc71237e0404259073c5c318cbd61
rudenkiy <sergiy.rudenkiy@digits.schwarzz>
2026-03-29 17:10:55 +0200

    ugly but working solution

--- 1594-max-non-negative-product/main.go
+++ 1594-max-non-negative-product/main.go
@@ -6,111 +6,81 @@ import (
 )
 
 func main() {
-	grid := [][]int{{-1, -2, -3}, {-2, -3, -3}, {-3, -3, -2}}
+	grid := [][]int{
+		{-1, -2, -3},
+		{-2, -3, -3},
+		{-3, -3, -2},
+	}
 
 	fmt.Println(maxProductPath(grid))
 }
 
-type point struct {
-	x, y int
-}
-
-type product struct {
-	min, max int
-}
-
-type queue []point
-
-func (q *queue) enqueue(value point) {
-	*q = append(*q, value)
-}
-
-func (q *queue) dequeue() (point, bool) {
-	if len(*q) == 0 {
-		return point{}, false
-	}
-	value := (*q)[0]
-	*q = (*q)[1:]
-	return value, true
-}
-
 func maxProductPath(grid [][]int) int {
-	result := map[point]product{
-		{0, 0}: {
-			min: grid[0][0],
-			max: grid[0][0],
-		},
+	minList := make([]int, len(grid[0]))
+	maxList := make([]int, len(grid[0]))
+
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			l, t := false, false
+			var leftmin, leftmax, topmin, topmax int
+			if j != 0 {
+				l = true
+				leftmin = minList[j-1]
+				leftmax = maxList[j-1]
+			}
+			if i != 0 {
+				t = true
+				topmin = minList[j]
+				topmax = maxList[j]
+			}
+
+			minList[j], maxList[j] = minMax(l, t, grid[i][j], leftmin, leftmax, topmin, topmax)
+		}
 	}
 
-	que := queue{
-		point{0, 0},
-	}
-
-	for {
-		q, ok := que.dequeue()
-		if !ok {
-			break
-		}
-
-		if q.x+1 < len(grid) {
-			p := point{q.x + 1, q.y}
-			que.enqueue(p)
-
-			cur := result[q]
-			var next *product
-			if n, ok := result[p]; ok {
-				next = &n
-			}
-			result[p] = checkPoint(grid[p.y][p.x], cur, next)
-
-		}
-		if q.y+1 < len(grid[0]) {
-			p := point{q.x, q.y + 1}
-			que.enqueue(p)
-			cur := result[q]
-			var next *product
-			if n, ok := result[p]; ok {
-				next = &n
-			}
-			if grid[p.y][p.x] != 0 {
-				result[p] = checkPoint(grid[p.y][p.x], cur, next)
-			}
-		}
-
-	}
-	res := result[point{len(grid[0]) - 1, len(grid) - 1}]
-
-	if res.max < 0 {
+	if maxList[len(grid[0])-1] < 0 {
 		return -1
 	}
-
 	mod := int(math.Pow10(9) + 7)
-
-	return res.max % mod
+	return maxList[len(grid[0])-1] % mod
 }
 
-func checkPoint(val int, cur product, next *product) product {
-	ma := cur.min * val
-	mi := cur.max * val
-
-	if ma < mi {
-		ma, mi = mi, ma
+func minMax(l, t bool, cur, leftmin, leftmax, topmin, topmax int) (newmin, newmax int) {
+	if !l && !t {
+		return cur, cur
+	}
+	newmin = leftmin * cur
+	newmax = leftmax * cur
+	if !l {
+		newmin = topmin * cur
+		newmax = topmax * cur
+	}
+	if newmin > newmax {
+		newmin, newmax = newmax, newmin
 	}
 
-	if next == nil {
-		return product{
-			min: mi,
-			max: ma,
+	if l {
+		lmin := leftmin * cur
+		lmax := leftmax * cur
+		if lmin > lmax {
+			lmin, lmax = lmax, lmin
+		}
+		newmin = lmin
+		newmax = lmax
+	}
+	if t {
+		tmin := topmin * cur
+		tmax := topmax * cur
+		if tmin > tmax {
+			tmin, tmax = tmax, tmin
+		}
+		if newmin > tmin {
+			newmin = tmin
+		}
+		if newmax < tmax {
+			newmax = tmax
 		}
 	}
 
-	if next.max < ma {
-		next.max = ma
-	}
-
-	if next.min > mi {
-		next.min = mi
-	}
-
-	return *next
+	return newmin, newmax
 }
