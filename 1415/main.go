be436a731e32621f225cb3bf2b4549a256247683
rudenkiy <sergiy.rudenkiy@digits.schwarzz>
2026-04-08 10:27:19 +0200

    add readme

--- 1415/main.go
+++ 1415/main.go
@@ -11,8 +11,7 @@ func main() {
 }
 
 func getHappyString(n int, k int) string {
-
-	if n == 0 || 3 * int(math.Pow(2, float64(n-1))) < k {
+	if n == 0 || 3*int(math.Pow(2, float64(n-1))) < k {
 		return ""
 	}
 
@@ -22,66 +21,64 @@ func getHappyString(n int, k int) string {
 }
 
 func happyStings(happy []string, n int) []string {
-	defaultStrings :=[]string{"a", "b", "c"}
-	
+	defaultStrings := []string{"a", "b", "c"}
+
 	if n == 1 {
 		return defaultStrings
 	}
 
 	unhappy := happyStings(happy, n-1)
 
-	resultStrings := make([]string, len(unhappy) * 2) 
+	resultStrings := make([]string, len(unhappy)*2)
 	for i := range unhappy {
-		switch { 
+		switch {
 		case strings.HasSuffix(unhappy[i], "a"):
-			resultStrings[2*i] = unhappy[i]+"b"
-			resultStrings[2*i+1] = unhappy[i]+"c"
+			resultStrings[2*i] = unhappy[i] + "b"
+			resultStrings[2*i+1] = unhappy[i] + "c"
 		case strings.HasSuffix(unhappy[i], "b"):
-			resultStrings[2*i] = unhappy[i]+"a"
-			resultStrings[2*i+1] = unhappy[i]+"c"
+			resultStrings[2*i] = unhappy[i] + "a"
+			resultStrings[2*i+1] = unhappy[i] + "c"
 		case strings.HasSuffix(unhappy[i], "c"):
-			resultStrings[2*i] = unhappy[i]+"a"
-			resultStrings[2*i+1] = unhappy[i]+"b"
+			resultStrings[2*i] = unhappy[i] + "a"
+			resultStrings[2*i+1] = unhappy[i] + "b"
 		}
 	}
 	return resultStrings
 }
 
-
-
 // solutionTwo calculate by the incoming number K
 // ["aba", "abc", "aca", "acb", "bab", "bac", "bca", "bcb", "cab", "cac", "cba", "cbc"]
-// as the list ordered, 0-4 starts always from a, 4-8 from b and 8-12 from c, 
+// as the list ordered, 0-4 starts always from a, 4-8 from b and 8-12 from c,
 // so 9 / 4 gives 3 - > firs letter is "c", reminder 1
-//  1 / 2 gives  0 -> we need to take  myMap["c"] from prev result and take element 0, reminder is 1
-// repeating, 1/2 gives  0 -> we need to take myMap["a"][0], "a" is from prev steep 
+//
+//	1 / 2 gives  0 -> we need to take  myMap["c"] from prev result and take element 0, reminder is 1
+//
+// repeating, 1/2 gives  0 -> we need to take myMap["a"][0], "a" is from prev steep
 func solutionTwo(n, k int) string {
-
-	if n == 0 || 3 * int(math.Pow(2, float64(n-1))) < k {
+	if n == 0 || 3*int(math.Pow(2, float64(n-1))) < k {
 		return ""
 	}
 
-	var result string 
+	var result string
 
 	defaultString := []string{"a", "b", "c"}
 
-	myMap  := map[string][]string{
-		"a": []string{"b", "c"},
-		"b": []string{"a", "c"},	
-		"c": []string{"a", "b"},	
-
+	myMap := map[string][]string{
+		"a": {"b", "c"},
+		"b": {"a", "c"},
+		"c": {"a", "b"},
 	}
 
-	rest := (k-1) / int(math.Pow(2, float64(n-1)))
-	reminder := (k-1) % int(math.Pow(2, float64(n-1)))
+	rest := (k - 1) / int(math.Pow(2, float64(n-1)))
+	reminder := (k - 1) % int(math.Pow(2, float64(n-1)))
 	result += defaultString[rest]
-    key := result
+	key := result
 	for i := 1; i < n; i++ {
 		rest = reminder / int(math.Pow(2, float64(n-1-i)))
 		reminder = reminder % int(math.Pow(2, float64(n-1-i)))
 		result += myMap[key][rest]
 		key = myMap[key][rest]
-	} 
+	}
 
 	return result
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+# My solution of the leetcode problems in golang
